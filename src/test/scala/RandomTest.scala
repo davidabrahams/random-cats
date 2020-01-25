586e33c3796ac1575297c8d40abb145cebc79531
davidabrahams <da.j.abrahams@gmail.com>
2020-01-24 18:36:33 -0800

    Add scalatest and scalacheck (#3)
    * dont ask me why this works
    
    * format
    
    * add a test
    
    * run tests in travis
    
    Co-authored-by: davidabrahams-stripe <42357729+davidabrahams-stripe@users.noreply.github.com>

---
+++ src/test/scala/RandomTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSuite
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class StackSpec extends FunSuite with ScalaCheckPropertyChecks {
+  test("This is a test") {
+    assert(true)
+  }
+}
