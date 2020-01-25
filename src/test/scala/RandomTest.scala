1ef013487caa03f5d541963e7098a8c9039ae85b
davidabrahams <da.j.abrahams@gmail.com>
2020-01-25 01:43:37 -0800

    implement nextDouble, nextBytes, nextFloat (#7)
    * implement nextDouble, nextBytes, nextFloat
    
    * disable while wart
    
    Co-authored-by: davidabrahams-stripe <42357729+davidabrahams-stripe@users.noreply.github.com>

--- src/test/scala/RandomTest.scala
+++ src/test/scala/RandomTest.scala
@@ -5,19 +5,45 @@ import java.util.{Random => JRandom}
 
 class RandomTest extends FunSuite with ScalaCheckDrivenPropertyChecks {
 
+  /**
+   * Tests that successive calls to produce a random A using Java's RNG and our own produce
+   * identical values
+   */
+  @SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
   private def testSuccessive[A](
       calls: Int,
       jRng: JRandom,
       sRng: Random,
       jFunc: JRandom => A,
-      sFunc: Random => (Random, A)
+      sFunc: Random => (Random, A),
+      // by default we use universal equality, but we may want to override (eg, to compare Arrays)
+      compare: (A, A) => Boolean = (a0: A, a1: A) => a0 == a1
   ): Unit = {
     var scalaRng = sRng
     for (_ <- 0 until calls) {
       val (rng0, scalaA) = sFunc(scalaRng)
       val javaA = jFunc(jRng)
       scalaRng = rng0
-      assert(scalaA == javaA)
+      assert(compare(scalaA, javaA))
+    }
+  }
+
+  test("matches Java nextBytes") {
+    forAll(Arbitrary.arbitrary[Long], Gen.choose(0, 20), Gen.choose(0, 100)) {
+      (l: Long, calls: Int, numBytes: Int) =>
+        val rng: Random = Random(Seed(l))
+        val javaRng: JRandom = new JRandom(l)
+        testSuccessive[Array[Byte]](
+          calls,
+          javaRng,
+          rng, { j =>
+            val bytes = new Array[Byte](numBytes)
+            j.nextBytes(bytes)
+            bytes
+          },
+          Random.nextBytes(numBytes),
+          (a0: Array[Byte], a1: Array[Byte]) => (a0.sameElements(a1))
+        )
     }
   }
 
@@ -25,7 +51,7 @@ class RandomTest extends FunSuite with ScalaCheckDrivenPropertyChecks {
     forAll(Arbitrary.arbitrary[Long], Gen.choose(0, 20)) { (l: Long, calls: Int) =>
       val rng: Random = Random(Seed(l))
       val javaRng: JRandom = new JRandom(l)
-      testSuccessive(calls, javaRng, rng, j => j.nextInt, Random.nextInt)
+      testSuccessive[Int](calls, javaRng, rng, j => j.nextInt, Random.nextInt)
     }
   }
 
@@ -33,7 +59,23 @@ class RandomTest extends FunSuite with ScalaCheckDrivenPropertyChecks {
     forAll(Arbitrary.arbitrary[Long], Gen.choose(0, 20)) { (l: Long, calls: Int) =>
       val rng: Random = Random(Seed(l))
       val javaRng: JRandom = new JRandom(l)
-      testSuccessive(calls, javaRng, rng, j => j.nextLong, Random.nextLong)
+      testSuccessive[Long](calls, javaRng, rng, j => j.nextLong, Random.nextLong)
+    }
+  }
+
+  test("matches Java nextDouble on successive calls") {
+    forAll(Arbitrary.arbitrary[Long], Gen.choose(0, 20)) { (l: Long, calls: Int) =>
+      val rng: Random = Random(Seed(l))
+      val javaRng: JRandom = new JRandom(l)
+      testSuccessive[Double](calls, javaRng, rng, j => j.nextDouble, Random.nextDouble)
+    }
+  }
+
+  test("matches Java nextFloat on successive calls") {
+    forAll(Arbitrary.arbitrary[Long], Gen.choose(0, 20)) { (l: Long, calls: Int) =>
+      val rng: Random = Random(Seed(l))
+      val javaRng: JRandom = new JRandom(l)
+      testSuccessive[Float](calls, javaRng, rng, j => j.nextFloat, Random.nextFloat)
     }
   }
 
