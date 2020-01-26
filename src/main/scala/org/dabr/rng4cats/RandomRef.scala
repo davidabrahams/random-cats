0be5182dc4dcfec2ba177a82c602ac9202348e31
David Abrahams <davidabrahams@stripe.com>
2020-01-26 13:31:37 -0800

    some comments

--- src/main/scala/org/dabr/rng4cats/RandomRef.scala
+++ src/main/scala/org/dabr/rng4cats/RandomRef.scala
@@ -12,14 +12,16 @@ trait RandomRef[F[_]] {
 
   /**
    * Generate a random A, and advance our our random state. It is expected that you use map,
-   * flatMap, and product to produce a program requiring multiple random numbers.
+   * flatMap, and product to produce a program requiring multiple random numbers. This is meant to
+   * interop with the functions defined on the [[Random]] companion, eg [[Random.nextLong]].
    */
   def use[A, B](f: (Random => (Random, A))): F[A]
 
   /**
-   * Check the current state. This is only for testing purposes in rng4cats
+   * Check the current Random state. It is recommended you only use this for debugging and logging,
+   * and use [[use]] when you actually need to produce random numbers.
    */
-  protected[rng4cats] def read: F[Random]
+  def read: F[Random]
 }
 
 final private class RandomRefImpl[F[_]](rngRef: Ref[F, Random]) extends RandomRef[F] {
