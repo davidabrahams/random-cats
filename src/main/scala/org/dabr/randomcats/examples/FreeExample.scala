bf65c69027727fcca006a9cd51f30dacd1fb75de
David Abrahams <da.j.abrahams@gmail.com>
2020-01-30 00:14:21 -0800

    Implement parallelWrite with both tagless and free

--- src/main/scala/org/dabr/randomcats/examples/FreeExample.scala
+++ src/main/scala/org/dabr/randomcats/examples/FreeExample.scala
@@ -1,6 +1,6 @@
 package org.dabr.randomcats.examples
 
-import cats._
+import cats.{Monad, Applicative, Parallel}
 import cats.effect._
 import cats.implicits._
 import cats.effect.concurrent._
@@ -10,7 +10,26 @@ import scala.concurrent._
 import org.dabr.randomcats._
 
 object FreeRandom {
-  sealed trait RandomDSL[A]
+
+  import HashedStoreDSL._
+
+  /**
+   * Returns true if all writes were successful
+   */
+  def parallelWrite[F[_]: Monad: Parallel](
+      writes: List[(Key, Value)],
+      rngRef: RandomRef[F]
+  ): F[HashedStoreApplicative[Boolean]] =
+    writes
+      .parTraverse {
+        case (k, v) =>
+          rngRef.use(Random.nextLong).map { l =>
+            HashedStoreDSL.putAp(k, new Hash(l), v)
+          }
+      }
+      .map { list =>
+        list.sequence.map(l => l.forall(identity))
+      }
 
 }
 
--- src/main/scala/org/dabr/randomcats/examples/HashedStoreDSL.scala
+++ src/main/scala/org/dabr/randomcats/examples/HashedStoreDSL.scala
@@ -1,19 +1,24 @@
 package org.dabr.randomcats.examples
 
 import cats.{~>, Monad, Applicative, Functor}
-import cats.free._
-import cats.free.Free._
+import cats.free.Free.liftF
+import cats.free.{Free, FreeApplicative}
 
 object HashedStoreDSL {
-  type FreeHashedStore[A] = Free[HashedStoreDSL, A]
+  type HashedStoreMonad[A] = Free[HashedStoreDSL, A]
+  type HashedStoreApplicative[A] = FreeApplicative[HashedStoreDSL, A]
   sealed trait HashedStoreDSL[A]
   final case class Put(k: Key, hash: Hash, v: Value) extends HashedStoreDSL[Boolean]
   final case class Get(k: Key) extends HashedStoreDSL[Option[Value]]
   final case class Hashes(k: Key) extends HashedStoreDSL[List[Hash]]
 
-  def put(k: Key, hash: Hash, v: Value): FreeHashedStore[Boolean] = liftF(Put(k, hash, v))
-  def get(k: Key): FreeHashedStore[Option[Value]] = liftF(Get(k))
-  def hashes(k: Key): FreeHashedStore[List[Hash]] = liftF(Hashes(k))
+  def put(k: Key, hash: Hash, v: Value): HashedStoreMonad[Boolean] = liftF(Put(k, hash, v))
+  def get(k: Key): HashedStoreMonad[Option[Value]] = liftF(Get(k))
+  def hashes(k: Key): HashedStoreMonad[List[Hash]] = liftF(Hashes(k))
+  def putAp(k: Key, hash: Hash, v: Value): HashedStoreApplicative[Boolean] =
+    FreeApplicative.lift(Put(k, hash, v))
+  def getAp(k: Key): HashedStoreApplicative[Option[Value]] = FreeApplicative.lift(Get(k))
+  def hashesAp(k: Key): HashedStoreApplicative[List[Hash]] = FreeApplicative.lift(Hashes(k))
   // def get(k: Key) extends HashedStoreDSL[Option[Value]]
   // def hashes(k: Key) extends HashedStoreDSL[List[Hash]]
   //
