76657069bddf8fcd7c34315e9b0b8a4d05f2c635
David Abrahams <da.j.abrahams@gmail.com>
2020-02-02 00:08:33 -0800

    clean up free example

--- src/main/scala/org/dabr/randomcats/examples/FreeExample.scala
+++ src/main/scala/org/dabr/randomcats/examples/FreeExample.scala
@@ -1,6 +1,8 @@
 package org.dabr.randomcats.examples
 
-import cats.{Monad, Applicative, Parallel, InjectK, ~>, Inject}
+import cats.tagless._
+
+import cats.{Monad, Applicative => Ap, Parallel, InjectK, ~>, Inject}
 import cats.data.EitherK
 import cats.free.{Free, FreeApplicative => FreeAp}
 import cats.effect._
@@ -14,36 +16,33 @@ import org.dabr.randomcats._
 
 object FreeRandom {
 
+  // TODO: unclear if this will ever be useful. We can define it though
+  implicit def fkFree[A]: FunctorK[Lambda[B[_] => Free[B, A]]] =
+    new FunctorK[Lambda[B[_] => Free[B, A]]] {
+      def mapK[F[_], G[_]](af: Free[F, A])(fk: F ~> G): Free[G, A] = af.mapK(fk)
+    }
+
   sealed trait RandomDSL[A]
   case object RandomLong extends RandomDSL[Long]
 
   import HashedStoreDSL._
 
-  type FreeS[F[_], A] = Free[({ type L[B] = FreeAp[F, B] })#L, A]
+  type FreeS[F[_], A] = Free[FreeAp[F, *], A]
   object FreeS {
     type Par[F[_], A] = FreeAp[F, A]
   }
 
   type DSL[A] = EitherK[RandomDSL, HashedStoreDSL, A]
-  type Program[A] = Free[DSL, A]
-  type ParProgram[A] = FreeS.Par[DSL, A]
-  type ParallelProgram[A] = FreeS[DSL, A]
 
-  /**
-   * This could be even more generic, on some G[_[_]]: FunctorK, and any F[_]: Applicative
-   */
-  def liftAp[F[_], A](free: Free[F, A]): FreeS[F, A] = {
-    type FreeApPartApplied[B] = FreeAp[F, B]
-    free.mapK {
-      new (F ~> ({ type G[B] = FreeAp[F, B] })#G) {
-        def apply[A](fa: F[A]): FreeAp[F, A] = FreeAp.lift(fa)
-      }
+  def freeApFunctionK[F[_], A]: F ~> FreeAp[F, *] =
+    new (F ~> FreeAp[F, *]) {
+      def apply[B](fa: F[B]): FreeAp[F, B] = FreeAp.lift(fa)
     }
-  }
+  def liftAp[F[_], A](free: Free[F, A]): FreeS[F, A] = free.mapK(freeApFunctionK)
 
   def parallelWriteFree(
       writes: List[(Key, Value)]
-  ): ParallelProgram[Boolean] = {
+  ): FreeS[DSL, Boolean] = {
     // first, get RNG values for each write in sequence using Monad
     val writesWithRng: Free[DSL, List[(Key, Value, Hash)]] = writes.traverse {
       case (k, v) =>
@@ -71,7 +70,7 @@ object FreeRandom {
    */
   def sequentialWriteFree(
       writes: List[(Key, Value)]
-  ): Program[Boolean] =
+  ): Free[DSL, Boolean] =
     writes
       .traverse {
         case (k, v) =>
@@ -93,7 +92,7 @@ object FreeRandom {
    * Returns true if all writes were successful
    * Make sure you pass an Applicative F here
    */
-  def parallelWrite[F[_]: Applicative](
+  def parallelWrite[F[_]: Ap](
       writes: List[(Key, Value)],
       rngRef: RandomRef[F]
   ): F[HashedStoreApplicative[Boolean]] =
